7d0e1fa3a89acbd4a4b8d49add3489d0b88cc6b9
Genius-Web <geniusweb328@gmail.com>
2022-04-22 20:11:43 -0500

    commit

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="body vh-100">
+    <button v-show="texto || clase" v-on:click="volver">
+      <b-row align-h="end" class="volver">
+        <b-col cols="5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="bi bi-arrow-left-circle-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+            /></svg
+        ></b-col>
+      </b-row>
+    </button>
     <div class="logo_nombre">
       <b-container>
         <b-row align-v="center">
@@ -38,12 +53,34 @@
     <b-container>
       <b-row class="justify-content-center">
         <b-col class="text-center" cols="10">
-          <div class="card" v-for="(show, index) in arrayFiltrado" :key="index">
-            <img :src="show.img" class="card-img-top" />
-            <div class="card-body">
-              <h1 class="card-title">{{ show.tipo }}</h1>
+          <transition-group name="list">
+            <div
+              v-show="!texto && !clase"
+              class="card"
+              v-for="show in categorias"
+              :key="show.tipo"
+            >
+              <button v-on:click="click(show.tipo)">
+                <img :src="show.img" class="card-img-top" />
+                <div class="card-body">
+                  <h1 class="card-title">{{ show.tipo }}</h1>
+                </div>
+              </button>
             </div>
-          </div>
+            <div
+              class="card"
+              v-for="(show, index) in arrayFiltrado"
+              :key="index + 1"
+              v-show="show.tipo == clase || texto"
+            >
+              <img :src="show.img" class="card-img-top" />
+              <div class="card-body">
+                <h1 class="card-title">{{ show.nombre }}</h1>
+              </div>
+            </div>
+          </transition-group>
+
+          <!-- En caso de no existir el producto muestre un aviso -->
           <div v-if="vacio" class="card">
             <img src="../img/search.svg" class="vacio" />
             <div class="card-body">
@@ -65,22 +102,29 @@ export default {
   },
   data() {
     return {
-      hola: ["1111", "2222", "3333"],
       busqueda: "",
       productos: [
         {
-          nombre: "Hola ",
-          tipo: 0,
-          edad: 60,
+          id: 1,
+          nombre: "Café",
+          tipo: "Bebidas calientes",
+          img: require("../img/cafe.jpg"),
         },
         {
-          nombre: "Hola2 ",
-          tipo: "Postres",
-          edad: 1,
+          id: 2,
+          nombre: "Mocca",
+          tipo: "Bebidas calientes",
+          img: require("../img/mocca.jpg"),
+        },
+        {
+          id: 3,
+          nombre: "Tinto",
+          tipo: "Bebidas calientes",
+          img: require("../img/tinto.jpg"),
         },
       ],
       categorias: [
-        { tipo: "Bebidas calientes", img: require("../img/cafe.jpg") },
+        { tipo: "Bebidas calientes", img: require("../img/cafe1.jpg") },
         { tipo: "Postres", img: require("../img/helado.jpg") },
         { tipo: "Licores", img: require("../img/vino.jpg") },
         { tipo: "Parva", img: require("../img/pan.jpg") },
@@ -88,10 +132,12 @@ export default {
       arrayFiltrado: [],
       texto: "",
       loaded: false,
+      vacio: false,
+      clase: "",
     };
   },
   created() {
-    this.arrayFiltrado = this.categorias;
+    this.arrayFiltrado = this.productos;
   },
   computed: {
     filtro: {
@@ -99,13 +145,36 @@ export default {
         return this.texto;
       },
       set(value) {
-        console.log(value);
         this.texto = value;
-        value = value.toLowerCase();
-        this.arrayFiltrado = this.categorias.filter(
-          (nota) => nota.tipo.toLowerCase().indexOf(value) !== -1
+        value = this.removeAccents(value);
+        this.arrayFiltrado = this.productos.filter(
+          (item) =>
+            item.nombre
+              .toLowerCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .indexOf(value) !== -1
         );
+        if (this.arrayFiltrado.length == 0) {
+          this.vacio = true;
+        } else {
+          this.vacio = false;
+        }
       },
+    },
+  },
+  methods: {
+    click(value) {
+      this.clase = value;
+    },
+    volver() {
+      this.clase = "";
+      this.texto = "";
+    },
+    removeAccents(str) {
+      str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      str.toLowerCase();
+      return str;
     },
   },
 };
@@ -121,7 +190,16 @@ export default {
   padding: 0;
   margin: 0;
 }
-
+.volver {
+  width: 50%;
+  position: fixed;
+  right: 7vw;
+  bottom: 6vh;
+  z-index: 999;
+}
+.volver svg {
+  color: #f7991d;
+}
 .logo_nombre {
   padding-top: 2%;
   padding-left: 4%;
@@ -212,5 +290,17 @@ export default {
 
 .card h1 {
   font-family: "Swis721 Lt BT";
+}
+button {
+  border: none;
+  background: none;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(-30px);
 }
 </style>
